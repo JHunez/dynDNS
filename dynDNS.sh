376981992a0325e884d0166c0d90e3373c53a4f3
Julien Hunez <git@hunez.fr>
2020-12-15 19:26:57 +0100

    Update dynDNS.sh

--- dynDNS.sh
+++ dynDNS.sh
@@ -5,16 +5,19 @@
 
 # Variables :
 ## obviously the name of my web server
-domainNameTested=""
+domainNameTested="toto.exemple.org"
+domainName="exemple.org"
 ## the server who gives me the IP actualy bind whith my $domainNameTested
-NSserver=""
+NSserver="NS.dnsprovider.com"
 ## where the log is save + name of the log
-logPath=""
-logName=""
+logPath="/some/logdir/"
+logName="DynDNS.log"
 log=$logPath$logName
+
 ## This is the tricky part : My dns provider's API aks a API key and a url.
-APIkey=""
-APIurl=""
+## Your's will be diffrent !
+APIkey="AGoogAPIKey"
+APIurl="https://api.dnsprovider.com/$domainName/record/toto"  
 
 # I check my public IP. https://api.myip.com/ give you a json response. Some regex get the IP.
 realip=$(curl https://api.myip.com/ | grep -Po '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
@@ -22,10 +25,19 @@ realip=$(curl https://api.myip.com/ | grep -Po '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,
 # Then I get what IP give me my dns provider for my web server
 dnsip=$(dig $domainNameTested +short @$NSserver)
 
+# The Content part of the API call. here JSON because my DNS provider request it
+# Be careful whit the quotes and the escapes charaters. Use and abuse of echo $data to be sure it is a JSON compliant content.
+data="{\"items\":[{\"rrset_type\":\"A\",\"rrset_values\":[\"$realip\"]}]}"
+
 # if the 2 values aren't the same do something !
 if [[ $dnsip != $realip ]] 
 then
-echo "$(date) : Public IP update to $realip" > $log
+echo "$(date) : Public IP update to $realip" >> $log
 # The API call it self. You have to check with your DNS provider how to do that.
-curl -X PUT -k -H "Authorization: Apikey $APIkey" -H 'Content-Type: application/json' -i "$APIurl" --data '{ "rrset_values" : ["'$realip'"] }' >> $log
+curl -X PUT -k -H "Authorization: Apikey $APIkey" -H 'Content-Type: application/json' -i "$APIurl" --data $data >> $log
+else
+echo "$(date) : Public IP ( $realip ) is the same as the DNS record $domainNameTested" >> $log
 fi
+
+# Add some empty line. Be pretty, you deserve it
+printf '\n\n\n' >> $log
